401d860b7c1c4505709c7160d26fcad795a29217
Cauã Pedrosa <cauapedrosa@live.com>
2016-08-16 11:35:19 -0300

    Updates (Main & GUI Structured & ++)

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,83 @@
 package controller;
 
-public class Main {
+import view.GUI;
 
+public class Main {
+	public static void main(String[] args) {
+		boolean loopFlag = true;
+
+		while (loopFlag) {
+
+			int flag = GUI.inputInt("============================================\n"
+					+ "Digite o numero correspondente a  operacao desejada:\n"
+					+ "============================================\n" + "0 - Sair\n"
+					+ "1 - Cadastrar Eleitor\n" + "2 - Cadastrar Partido\n"
+					+ "3 - cadastraCandidato\n" + "4 - cadastraZona\n"
+					+ "5 - cadastraSecao\n" + "6 - verificaCadastros\n");
+
+			switch (flag) {
+			case 0:
+				loopFlag = false;
+				break;
+
+			case 1:
+				cadastraEleitor();
+				break;
+
+			case 2:
+				cadastraPartido();
+				break;
+
+			case 3:
+				cadastraCandidato();
+				break;
+
+			case 4:
+				cadastraZona();
+				break;
+
+			case 5:
+				cadastraSecao();
+				break;
+
+			case 6:
+				verificaCadastros();
+				break;
+
+			default:
+				loopFlag = false;
+				break;
+			}
+		}
+	}
+
+	private static void verificaCadastros() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cadastraSecao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cadastraZona() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cadastraCandidato() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cadastraPartido() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void cadastraEleitor() {
+		// TODO Auto-generated method stub
+		
+	}
 }
---
+++ src/model/Zona.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Zona {
+	private int numero;
+	private String localizacao;
+	private ArrayList<Secao> secoes;
+	private int numeroSecao;
+
+	public Zona(int numero, String localizacao) {
+		this.numero = numero;
+		this.localizacao = localizacao;
+		this.secoes = new ArrayList<Secao>();
+		this.numeroSecao = 0;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public String getLocalizacao() {
+		return this.localizacao;
+	}
+
+	public Secao criarNovaSecao() {
+		this.numeroSecao++;
+		Secao secao = new Secao(this.numeroSecao, this);
+		this.secoes.add(secao);
+		return secao;
+	}
+}
